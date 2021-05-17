2ab0b7d2f52780be8ecd6856e80a77bb388da000
Michael Stubbs <michaelstubbsdev@gmail.com>
2021-05-17 14:47:14 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const helmet = require("helmet");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.static(__dirname + "/build"));
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.send("Connected to Express");
+});
+
+app.listen(port, () => console.log("server is listening on " + port));
+
+//Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
