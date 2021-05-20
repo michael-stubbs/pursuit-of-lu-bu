5faee5bd06a5ab090c27253396332eea032b655d
Michael Stubbs <michaelstubbsdev@gmail.com>
2021-05-20 15:49:19 -0700

    test for routing fix

--- routes/db.js
+++ routes/db.js
@@ -1,20 +1,22 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const router = express.Router();
 const MongoClient = require("mongodb").MongoClient;
 
 let reviews;
 const login = process.env.MONGO_USER + ":" + process.env.MONG_PASS;
+console.log(login);
 const uri =
   "mongodb+srv://" +
   login +
   "@cluster0.epqz8.mongodb.net/Cluster0?retryWrites=true&w=majority";
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 client.connect((err) => {
+  console.log(err);
   const collection = client.db("POLB").collection("Reviews");
   collection
     .find({})
